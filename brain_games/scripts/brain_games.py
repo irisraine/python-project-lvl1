cf4fdf28b6cc98ab5acd4103b719761d68536475
Iris-Raine <iris.raine.darkest.star@gmail.com>
2022-07-13 12:59:37 +0000

    fourth stage of project #1

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,6 +2,7 @@
 
 from brain_games.cli import welcome_user
 
+
 def main():
     print("Welcome to the Brain Games!")
     welcome_user()
@@ -9,4 +10,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
