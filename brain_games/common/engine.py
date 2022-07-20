32f253160e153655468d91ef2e0b4e6e84c6e507
Iris-Raine <iris.raine.darkest.star@gmail.com>
2022-07-20 14:09:21 +0000

    Второй этап общих доработок

--- brain_games/common/engine.py
+++ brain_games/common/engine.py
@@ -1,39 +1,23 @@
 import prompt
 
 
-def welcome_user():
+# Запуск полного игрового цикла
+def launch(game):
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
-    return name
-
-
-def launch(game):
-    name = welcome_user()
     print(game.GREET_MESSAGE)
-    guessing_loop(name, game.stage)
-
-
-def guessing_loop(name, stage):
     number_of_correct_answers = 0
     for _ in range(3):
-        question, correct_answer = stage()
+        question, correct_answer = game.single_game_stage()
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
-        if (is_correct_answer(answer, correct_answer)):
+        if answer == correct_answer:
             number_of_correct_answers += 1
+            print('Correct!')
         else:
+            print(f"'{answer}' is wrong answer ;(.", end=" ")
+            print(f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             break
-
-    if (number_of_correct_answers == 3):
+    if number_of_correct_answers == 3:
         print(f"Congratulations, {name}!")
-
-
-def is_correct_answer(answer, correct_answer):
-    if (answer == correct_answer):
-        print('Correct!')
-        return True
-    else:
-        print(f"'{answer}' is wrong answer ;(.", end=" ")
-        print(f"Correct answer was '{correct_answer}'.")
