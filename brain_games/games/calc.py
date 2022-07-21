acf1bc2a5ba37d67c3b6460bfb0c1cc627964899
Iris-Raine <iris.raine.darkest.star@gmail.com>
2022-07-21 06:13:32 +0000

    third stage of common improvements

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -3,21 +3,22 @@ from random import randint
 
 GREET_MESSAGE = 'What is the result of the expression?'
 
+NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT = 1, 100
 
-# Единичный этап игры
+
 def single_game_stage():
-    NUMBER_LOWER_LIMIT = 1
-    NUMBER_UPPER_LIMIT = 100
-
+    """Единичный этап игры. """
     arithmetic_operations = ['+', '-', '*']
     first_number = randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
     second_number = randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
     arithmetic_operation = arithmetic_operations[randint(0, 2)]
     question = f'{first_number} {arithmetic_operation} {second_number}'
+
     if arithmetic_operation == "+":
         correct_answer = first_number + second_number
     elif arithmetic_operation == "-":
         correct_answer = first_number - second_number
     elif arithmetic_operation == "*":
         correct_answer = first_number * second_number
+
     return question, str(correct_answer)
--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -3,18 +3,17 @@ from random import randint
 
 GREET_MESSAGE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
+NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT = 1, 100
 
-# Единичный этап игры
+
 def single_game_stage():
-    NUMBER_LOWER_LIMIT = 1
-    NUMBER_UPPER_LIMIT = 100
-
+    """Единичный этап игры. """
     question = randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
     correct_answer = "yes" if is_even(question) else "no"
-    return question, correct_answer
+
+    return str(question), correct_answer
 
 
-# Проверка числа на четность
 def is_even(number):
-    if number % 2 == 0:
-        return True
+    """Проверка числа на четность. """
+    return number % 2 == 0
