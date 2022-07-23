4ab3bd5c88bc6b68b4e01a3d878e85d6090e5545
Iris-Raine <iris.raine.darkest.star@gmail.com>
2022-07-23 13:59:11 +0000

    change method of selecting of arithmetic expression

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 
 GREET_MESSAGE = 'What is the result of the expression?'
@@ -11,7 +11,7 @@ def single_game_stage():
     arithmetic_operations = ['+', '-', '*']
     first_number = randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
     second_number = randint(NUMBER_LOWER_LIMIT, NUMBER_UPPER_LIMIT)
-    arithmetic_operation = arithmetic_operations[randint(0, 2)]
+    arithmetic_operation = choice(arithmetic_operations)
     question = f'{first_number} {arithmetic_operation} {second_number}'
 
     if arithmetic_operation == "+":
